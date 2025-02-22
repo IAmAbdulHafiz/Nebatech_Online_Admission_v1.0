afbe7496689a71b49704943e42811093311106cf
IAmAbdulHafiz <imabdulhafiz.yussif@gmail.com>
2025-02-22 02:19:37 +0000

    Update files

--- index.php
+++ index.php
@@ -10,10 +10,14 @@
     <link rel="icon" href="assets/images/favicon.ico">
     <title>Nebatech Software Solution Ltd</title>
     <!-- Bootstrap 5 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Font Awesome 6 -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <!-- Custom CSS -->
+    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+--><!-- Font Awesome 6 -->
+    <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+--><!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/all.min.css">
+    
+
     <link rel="stylesheet" href="assets/css/style.css">
     <!-- AOS Animation Library -->
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
