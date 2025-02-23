8651917f6a0d1fd745398e04fc595e848783ef88
ABDUL-HAFIZ YUSSIF <imabdulhafiz.yussif@gmail.com>
2025-02-23 18:00:30 +0000

    Update Hubtel_payment

--- admission_portal/api/hubtel_payment.php
+++ admission_portal/api/hubtel_payment.php
@@ -41,7 +41,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     curl_setopt_array($ch, [
         CURLOPT_URL => 'https://payproxyapi.hubtel.com/items/initiate',
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_POST => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
         CURLOPT_POSTFIELDS => json_encode($postData),
         CURLOPT_HTTPHEADER => [
             'Content-Type: application/json',
