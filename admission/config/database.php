148855f07c3b8bdeef8696e3af1ab07daf660f53
IAmAbdulHafiz <imabdulhafiz.yussif@gmail.com>
2025-02-25 23:04:00 +0000

    Update

--- admission/config/database.php
+++ admission/config/database.php
@@ -1,16 +1,9 @@
 <?php
-include 'config/config.php';
-
 // Database configuration
-
-$host = getenv('DB_HOST');
-$db_name = getenv('DB_NAME');
-$username = getenv('DB_USERNAME');
-$password = getenv('DB_PASSWORD');
-
-//$db_name = "u948335622_nebatech";
-//$username = "u948335622_nebatech";
-//$password = "imAbdulHafiz@122996";
+$host = "localhost";
+$db_name = "u948335622_nebatech";
+$username = "u948335622_nebatech";
+$password = "imAbdulHafiz@122996";
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
