9d1a84650c2ce775d69aeccad5803bb96b64d877
IAmAbdulHafiz <imabdulhafiz.yussif@gmail.com>
2025-02-25 23:31:32 +0000

    Update file

--- admission/payment_form.php
+++ admission/payment_form.php
@@ -14,7 +14,7 @@ $checkoutUrl = htmlspecialchars($_SESSION['checkout_url']);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Complete Your Payment</title>
     <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <style>
         body {
