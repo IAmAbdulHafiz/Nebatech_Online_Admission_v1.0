0557eff38526d24249f29f37ed3c48e729fb7ed0
IAmAbdulHafiz <imabdulhafiz.yussif@gmail.com>
2025-02-25 22:26:25 +0000

    Update config & Database

--- config/database.php
+++ config/database.php
@@ -1,16 +1,20 @@
 <?php
-include("../api/config.php"); // Load environment variables
+require __DIR__ . '/../vendor/autoload.php'; // Ensure Composer's autoloader is loaded
 
-// Database configuration
+// Load environment variables
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+// Use environment variables for DB connection
 $host = getenv('DB_HOST');
-$db_name = getenv('DB_NAME');
+$dbname = getenv('DB_NAME');
 $username = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $exception) {
-    echo "Connection error: " . $exception->getMessage();
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
 }
 ?>
