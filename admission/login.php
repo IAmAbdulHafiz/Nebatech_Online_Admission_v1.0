dd2b9b541f9d1deafaca4f7bd10fffcb98d6ba49
IAmAbdulHafiz <imabdulhafiz.yussif@gmail.com>
2025-02-26 07:30:15 +0000

    Update files

--- admission/login.php
+++ admission/login.php
@@ -45,9 +45,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
   <style>
-    .body {
-      background: linear-gradient(135deg, #002060, #0056b3);
-    }
     .login-container {
       background: #fff;
       color: #333;
