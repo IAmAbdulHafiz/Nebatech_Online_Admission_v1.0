5f02469e159662037283e87845cb880c1fbf4bf2
IAmAbdulHafiz <imabdulhafiz.yussif@gmail.com>
2025-03-01 06:31:31 +0000

    Update file

--- config/database.php
+++ config/database.php
@@ -1,15 +1,8 @@
 <?php 
-require_once __DIR__ . '/vendor/autoload.php'; // Load Composer dependencies
-
-// Load environment variables from .env file
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-// Database configuration using environment variables
-$host = $_ENV['DB_HOST'];
-$db_name = $_ENV['DB_NAME'];
-$username = $_ENV['DB_USER'];
-$password = $_ENV['DB_PASS'];
+$host = getenv('DB_HOST');
+$db_name = getenv('DB_NAME');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
