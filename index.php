92bf518e3d9a30e355024684ecb8f99e3d6f2e67
IAmAbdulHafiz <imabdulhafiz.yussif@gmail.com>
2025-03-01 05:51:57 +0000

    Update files

--- index.php
+++ index.php
@@ -169,6 +169,19 @@
             showCursor: true
         });
     </script>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/65dccd3d9131ed19d971d067/1hnj83egi';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
     <!-- Footer will be included here -->
     <?php include 'includes/footer.php'; ?>
 </body>
