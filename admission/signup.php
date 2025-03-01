72013ed9074c226612c178f1cb194c809cf01fb1
IAmAbdulHafiz <imabdulhafiz.yussif@gmail.com>
2025-03-01 03:44:20 +0000

    Update file

--- admission/signup.php
+++ admission/signup.php
@@ -211,7 +211,9 @@ $pageType = 'signup';
   </div>
 
   <?php include 'includes/footer.php'; ?>
-
+  
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
   <script>
     // Real-time input validation for immediate feedback
     document.addEventListener('DOMContentLoaded', function() {
