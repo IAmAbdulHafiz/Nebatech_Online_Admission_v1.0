1036331335f6162145ba5caeca2963c95e073a6c
IAmAbdulHafiz <imabdulhafiz.yussif@gmail.com>
2025-03-02 07:24:26 +0000

    Update file

--- admission/submit_application.php
+++ admission/submit_application.php
@@ -1,4 +1,10 @@
 <?php 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 session_start();
 include('../config/database.php'); // Include database connection
 include('../includes/functions.php'); // Include helper functions
